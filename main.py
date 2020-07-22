1cffc23bbd19e888a0e815826081678c9c7fb280
FlyingThunder <laurens.peschko@gmail.com>
2020-07-22 16:33:30 +0200

    initial commit

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from discord.ext import commands
 import praw
 import json
 import urllib.request
+import asyncio
+
 #.env laden
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -18,7 +20,6 @@ def Mainbot():
     reddit = praw.Reddit(client_id='x',client_secret='x',user_agent='x')
     post = reddit.subreddit('okbrudimongo').random()
     x = post.id
-
 
     with open('data.json', 'r') as e:
         eread = e.read()
@@ -39,27 +40,60 @@ def Mainbot():
     file.close()
     return(post.url + " " + "\n" + post.title + " " + "\n" + "https://reddit.com/r/okbrudimongo/comments/"+x)
 
-@bot.command(name="Wissen")
+@bot.command(name="wissen", help="keckige witze")
 async def okbrudimongo_post(ctx):
     await ctx.send(Mainbot())
 
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
+    channel = bot.get_channel(733248970771660825)
+    await channel.send("Bin gelandet auf Aldebaran.")
 
-@bot.command(pass_context=True, name="Saubermachen", help="!clean x = letzte x nachrichten löschen")
+@bot.command(name="Magie=Physik/Wollen", help="löscht letzte x nachrichten im kanal")
 @commands.has_permissions(administrator=True)
 async def clean(ctx, limit: int):
         await ctx.channel.purge(limit=limit)
         await ctx.send('Cleared by {}'.format(ctx.author.mention))
         await ctx.message.delete()
 
+# @bot.command()
+# async def join(ctx):
+#         channel = ctx.message.author.voice.channel
+#         await channel.connect()
+#         return channel.connect()
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.errors.CheckFailure):
-        await ctx.send('You do not have the correct role for this command.')
+@bot.command()
+async def leave(ctx):
+        print(bot.voice_clients)
+        for x in bot.voice_clients:
+            if(x.guild == ctx.message.guild):
+                return await x.disconnect()
+
+@bot.command()
+async def alle(ctx):
+    voice_channel = ctx.message.author.voice.channel
+    if voice_channel != None:
+        channel = voice_channel.name
+        await ctx.send('User is in channel: ' + channel)
+        vc = await voice_channel.connect()
+        vc.play(discord.FFmpegPCMAudio('testing.mp3'), after=lambda e: print('done', e))
+        while vc.is_playing() == True:
+            pass
+        else:
+            for x in bot.voice_clients:
+                 if (x.guild == ctx.message.guild):
+                     return await x.disconnect()
+
+    else:
+        await ctx.send('User is not in a channel.')
+
+
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, commands.errors.CheckFailure):
+#         await ctx.send('You do not have the correct role for this command.')
 
 @bot.event
 async def on_message(message):
@@ -67,7 +101,11 @@ async def on_message(message):
         return
     if message.content in ["wie viele?", "Wie viele?"]:
         await message.channel.send("Alle.")
+    if "mama mia" in str(message.content).lower():
+        await message.channel.send(file=discord.File('mamamia.png'))
+
     await bot.process_commands(message)
+
 
 
 bot.run(TOKEN)
