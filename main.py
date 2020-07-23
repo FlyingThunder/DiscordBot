7827df033a9bde5d25fa4fa566e6dd856a240e11
FlyingThunder <laurens.peschko@gmail.com>
2020-07-23 10:13:18 +0200

    added cogs

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ TOKEN = os.getenv('DISCORD_TOKEN')
 #bot command präfix
 bot = commands.Bot(command_prefix='!')
 
+
 #reddit API laden
 def Mainbot():
     reddit = praw.Reddit(client_id='x',client_secret='x',user_agent='x')
@@ -37,22 +38,57 @@ def Mainbot():
     file.close()
     return(post.url + " " + "\n" + post.title + " " + "\n" + "https://reddit.com/r/okbrudimongo/comments/"+x)
 
-@bot.command(name="wissen", help="keckige witze")
-async def okbrudimongo_post(ctx):
-    await ctx.send(Mainbot())
+class EnergetischeMatrix(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
 
-@bot.event
-async def on_ready():
-    print(f'{bot.user.name} has connected to Discord!')
-    channel = bot.get_channel(733248970771660825)
-    await channel.send("Bin gelandet auf Aldebaran.")
+    """keckige mehmehs"""
 
-@bot.command(name="Magie=Physik/Wollen", help="löscht letzte x nachrichten im kanal")
-@commands.has_permissions(administrator=True)
-async def clean(ctx, limit: int):
+
+
+    @commands.command(help="keckige witze")
+    async def wissen(self, ctx):
+        await ctx.send(Mainbot())
+
+    @commands.command(help="Ein Gruß vom Doktor")
+    async def willkommen(self, ctx):
+        voice_channel = ctx.message.author.voice.channel
+        if voice_channel != None:
+            vc = await voice_channel.connect()
+            vc.play(discord.FFmpegPCMAudio('welcome.mp3'), after=lambda e: print('done', e))
+            while vc.is_playing() == True:
+                pass
+            else:
+                for x in bot.voice_clients:
+                    if (x.guild == ctx.message.guild):
+                        return await x.disconnect()
+
+class OberkommandoBefehle(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(help="löscht letzte x Nachrichten im Kanal")
+    @commands.has_permissions(administrator=True)
+    async def magie(self, ctx, limit: int):
         await ctx.channel.purge(limit=limit)
         await ctx.send('Cleared by {}'.format(ctx.author.mention))
         await ctx.message.delete()
+
+
+
+@bot.event
+async def on_ready():
+    text_channel_list = []
+    for guild in bot.guilds:
+        for channel in guild.channels:
+            if channel.category is not None:
+                if "Text Channels" in str(channel.category):
+                    text_channel_list.append(channel)
+
+    print(f'{bot.user.name} has connected to Discord!')
+    await text_channel_list[0].send("Bin gelandet auf Aldebaran.")
+
+
 
 # @bot.command()
 # async def join(ctx):
@@ -60,26 +96,12 @@ async def clean(ctx, limit: int):
 #         await channel.connect()
 #         return channel.connect()
 
-
-@bot.command()
-async def leave(ctx):
-        print(bot.voice_clients)
-        for x in bot.voice_clients:
-            if(x.guild == ctx.message.guild):
-                return await x.disconnect()
-
-@bot.command(help="Sprachkanalbefehl")
-async def willkommen(ctx):
-    voice_channel = ctx.message.author.voice.channel
-    if voice_channel != None:
-        vc = await voice_channel.connect()
-        vc.play(discord.FFmpegPCMAudio('welcome.mp3'), after=lambda e: print('done', e))
-        while vc.is_playing() == True:
-            pass
-        else:
-            for x in bot.voice_clients:
-                 if (x.guild == ctx.message.guild):
-                     return await x.disconnect()
+# @bot.command()
+# async def leave(ctx):
+#         print(bot.voice_clients)
+#         for x in bot.voice_clients:
+#             if(x.guild == ctx.message.guild):
+#                 return await x.disconnect()
 
 
 # @bot.event
@@ -122,5 +144,6 @@ async def on_message(message):
     await bot.process_commands(message)
 
 
-
+bot.add_cog(EnergetischeMatrix(bot))
+bot.add_cog(OberkommandoBefehle(bot))
 bot.run(TOKEN)
