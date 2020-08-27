0d0aaf7915c26fbd50094da530bfc58fe219f265
Laurens <laurens.peschko@gmail.com>
2020-08-27 22:58:17 +0200

    dropbox file sync

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from riotwatcher import LolWatcher, ApiError
 import urllib.request
 import youtube_dl
 from audioclipextractor import AudioClipExtractor, SpecsParser
+import dropbox
 
 
 
@@ -20,9 +21,11 @@ client_id_var = os.getenv("reddit_client_id")
 client_secret_var = os.getenv("reddit_client_secret")
 user_agent_var = os.getenv("reddit_user_agent")
 riot_api_key = os.getenv("riot_api_key")
+dropbox_key = os.getenv("dropbox_key")
 
 watcher = LolWatcher(riot_api_key)
 my_region = 'euw1'
+dbx = dropbox.Dropbox(dropbox_key)
 
 files = os.listdir('res')
 audiofiles = []
@@ -31,8 +34,6 @@ for x in files:
         audiofiles.append(x)
 print(audiofiles)
 
-
-
 #bot command präfix
 bot = commands.Bot(command_prefix='!',case_insensitive=True)
 bot.remove_command("help")
@@ -40,7 +41,33 @@ ffmpegpath = "res/ffmpeg.exe"
 startTime = datetime.now()
 print(startTime)
 
-#reddit API laden
+def dropbox_upload():
+    audiofiles_dropbox = []
+    dropbox_filescan = dbx.files_list_folder("/discordbotmp3s")
+    for x in dropbox_filescan.entries:
+        audiofiles_dropbox.append(x.name)
+
+    mp3_files = os.listdir("res/mp3s/")
+    print(mp3_files)
+    for x in mp3_files:
+        if x not in audiofiles_dropbox:
+            f = open('res/mp3s/{}'.format(x), 'rb')
+            dbx.files_upload(f.read(), "/DiscordBotMp3s/{}".format(x))
+
+def dropbox_download():
+    audiofiles_dropbox = []
+    dropbox_filescan = dbx.files_list_folder("/discordbotmp3s")
+    for x in dropbox_filescan.entries:
+        audiofiles_dropbox.append(x.name)
+    print(audiofiles_dropbox)
+
+    mp3_files = os.listdir("res/mp3s/")
+
+    for y in audiofiles_dropbox:
+        if y not in mp3_files:
+            with open("res/mp3s/{}".format(y), "wb") as f:
+                metadata, res = dbx.files_download(path="/DiscordBotMp3s/{}".format(y))
+                f.write(res.content)
 
 def champLookup(champId):
     latest = watcher.data_dragon.versions_for_region(my_region)['n']['champion']
@@ -170,7 +197,7 @@ def Last_10_games(name):
 async def Labern(audiofile, message):
    voice_channel = message.author.voice.channel
    vc = await voice_channel.connect()
-   vc.play(discord.FFmpegPCMAudio(source='res/{}.mp3'.format(audiofile.lower())))
+   vc.play(discord.FFmpegPCMAudio(source='res/mp3s/{}.mp3'.format(audiofile.lower())))
    while vc.is_playing() == True:
        pass
    else:
@@ -272,15 +299,18 @@ class Physik(commands.Cog):
 
                     specs = str(start) + " " + str(end)
                     ext.extract_clips(specs)
-                    os.rename('clip1.mp3', 'res/' + str(name).lower() + '.mp3')
+                    os.rename('clip1.mp3', 'res/mp3s/' + str(name).lower() + '.mp3')
                     os.remove('test.mp3')
                 else:
                     print("Youtubevideo runtergeladen von: " + str(ctx.author) + "[" + str(name) + "]")
-                    os.rename('test.mp3', 'res/' + str(name).lower() + '.mp3')
+                    os.rename('test.mp3', 'res/mp3s/' + str(name).lower() + '.mp3')
 
                 await ctx.send("YT Video " + str(url) + " runtergeladen unter dem Namen: " + str(name))
             except Exception as e:
                 await ctx.send("Es ist ein: " + str(e.__class__) + " Fehler aufgetreten.")
+
+        dropbox_upload()
+        await ctx.send("{} wurde in Dropbox hochgeladen".format(name))
 
     @commands.command(help="stats vong letzte 10 spiele her")
     async def Letzte10(self, ctx, argument):
@@ -299,9 +329,9 @@ class Physik(commands.Cog):
     async def add_audiofile(self, ctx, link, name=None):
         print(link)
         if name == None:
-            urllib.request.urlretrieve(link, "res/"+str(link.split("/")[-1]))
+            urllib.request.urlretrieve(link, "res/mp3s/"+str(link.split("/")[-1]))
         else:
-            urllib.request.urlretrieve(link, "res/" + str(name) + ".mp3")
+            urllib.request.urlretrieve(link, "res/mp3s/" + str(name) + ".mp3")
         await ctx.send("Audiodatei " + str(link.split("/")[-1]) + " hinzugefügt")
 
     @commands.command(help="SEID IHR BEREIT KINDER?")
@@ -337,9 +367,9 @@ class Magie(commands.Cog):
     # @commands.command(help="löscht audiodatei")
     # async def Löschung(self, ctx, name):
     #     print(name)
-    #     print(os._exists("res/"+str(name)+".mp3"))
+    #     print(os._exists("res/mp3s/"+str(name)+".mp3"))
     #     if ".mp3" in name:
-    #         os.remove("res/"+str(name)+".mp3")
+    #         os.remove("res/mp3s/"+str(name)+".mp3")
     #         await ctx.send(str(name) + " wurde gelöscht. Hurensohn.")
 
     @commands.command(help="löscht letzte x Nachrichten im Kanal")
@@ -359,24 +389,31 @@ class Magie(commands.Cog):
 
 @bot.event
 async def on_ready():
-    text_channel_list = []
     for guild in bot.guilds:
-        print(guild.id)
         if guild.id == 262510619503230976: #Shitheads
             for channel in guild.channels:
                 if channel.name == "general":
                     await channel.send("Bin gelandet auf Aldebaran.")
-                print(channel)
         if guild.id == 733248970771660822: #Bot Test
             for channel in guild.channels:
                 if channel.name == "general":
                     await channel.send("Bin gelandet auf Aldebaran.")
 
+    dropbox_download()
+
+    for guild in bot.guilds:
+        if guild.id == 262510619503230976: #Shitheads
+            for channel in guild.channels:
+                if channel.name == "general":
+                    await channel.send("mp3 Dateien wurden von Dropbox runtergeladen.")
+        if guild.id == 733248970771660822: #Bot Test
+            for channel in guild.channels:
+                if channel.name == "general":
+                    await channel.send("mp3 Dateien wurden von Dropbox runtergeladen.")
 
     print(f'{bot.user.name} has connected to {guild}')
     sys.stdout.flush()
-    #print(text_channel_list)
-    #await text_channel_list[0].send("Bin gelandet auf Aldebaran.")
+
 
 
 
